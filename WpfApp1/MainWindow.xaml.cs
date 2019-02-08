b86430f6ff702b1061352eee43c3a794bcce878a
peterlane17 <peterlane17@gmail.com>
2019-02-08 15:02:21 -0800

    Almost ready to publish...

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -25,13 +25,11 @@ namespace CustomerServiceComplaintAutomationTool
     {
         IWebDriver driver = new ChromeDriver();
         string fileName = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "WriteLine.txt");
-        
-
 
         public MainWindow()
         {
             InitializeComponent();
-            
+
 
             if (IsLoginInfoPresent() == true)
             {
@@ -55,7 +53,7 @@ namespace CustomerServiceComplaintAutomationTool
             {
                 return false;
             }
-            catch (Exception)
+            catch (Exception) //made for if the "WriteLine.txt" file doesn't exist
             {
                 return false;
             }
@@ -74,159 +72,160 @@ namespace CustomerServiceComplaintAutomationTool
             }
         }
 
+        private void Usernamex_TextChanged(object sender, TextChangedEventArgs e)
+        { }
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        { }
+        public void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        { }
+
+        public void Run_Click(object sender, RoutedEventArgs e)
         {
+
+            if (Checkbox1.IsChecked.Value)
+            {
                 using (System.IO.StreamWriter file = new System.IO.StreamWriter(fileName))
                 {
                     file.WriteLine(Usernamex.Text);
                     file.WriteLine(Passwordx.Password);
                     file.WriteLine(Emailx.Text);
                 }
-        }
-
-        public void CheckBox_Unchecked(object sender, RoutedEventArgs e) //if left unchecked it will delete txtfile containing login info
-            {                                                                      
-                if (System.IO.File.Exists(fileName))
+            }
+            else if (System.IO.File.Exists(fileName))
+            {
+                try
                 {
-                    try
-                    {
-                        System.IO.File.Delete(fileName);
-                    }
-                    catch (System.IO.IOException)
-                    {
-                        return;
-                    }
+                    System.IO.File.Delete(fileName);
+                }
+                catch (System.IO.IOException)
+                {
+                    return;
                 }
             }
-    
-        public void Run_Click(object sender, RoutedEventArgs e)
+
+            int complaintNumber = int.Parse(ComplaintNumberx.Text);
+            string magentouserName = Usernamex.Text;
+            string magentoPass = Passwordx.Password;
+            string workEmail = Emailx.Text;
+
+            driver.Url = "http://complaints/Home/ReadOnlyDetails/" + complaintNumber; //opens complaint webpage
+
+            //gets the customers info for replacement orders
+
+            var firstName = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[2]/td[2]")).Text;
+
+            var lastName = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[2]/td[3]")).Text;
+
+            var email = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[4]/td[1]")).Text;
+
+            var phoneNumber = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[4]/td[2]")).Text;
+
+            var country = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[4]/td[4]")).Text;
+
+            var streetAddress = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[1]")).Text;
+
+            var city = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[2]")).Text;
+
+            var state = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[3]")).Text;
+
+            var zipcode = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[4]")).Text;
+
+            var itemNumber = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[2]/table/tbody/tr[2]/td[1]")).Text;
+
+            //Beginning of Magento navigation
+
+            driver.Url = "https://www.bobsredmill.com/index.php/admin/";
+
+            var userName = driver.FindElement(By.XPath("//*[@id='username']")); //if using the app two times in a row this will throw an exception because you're already logged in!
+            userName.SendKeys(magentouserName);
+
+            var password = driver.FindElement(By.XPath("//*[@id='login']"));
+            password.SendKeys(magentoPass);
+
+            var magLogin = driver.FindElement(By.XPath("//*[@id='loginForm']/div/div[5]/input"));
+            magLogin.Click();
+
+            //navigating to one's own account
+
+            System.Threading.Thread.Sleep(2000);
+
+            var customersTab = driver.FindElement(By.XPath("//*[@id='nav']/li[4]"));
+            customersTab.Click();
+
+            System.Threading.Thread.Sleep(3000);
+
+            var manageCustomers = driver.FindElement(By.XPath("//*[@id='nav']/li[4]/ul/li[1]/a"));
+            manageCustomers.Click();
+
+            var csrepemailSearch = driver.FindElement(By.XPath("//*[@id='customerGrid_filter_email']"));
+            csrepemailSearch.SendKeys(workEmail);
+            csrepemailSearch.SendKeys(Keys.Return);
+
+            System.Threading.Thread.Sleep(4000); //NEEDED, otherwise the next line won't click the right element
+
+            var csrepemailEnter = driver.FindElement(By.ClassName("even"));
+            csrepemailEnter.Click();
+
+            var movetoAddress = driver.FindElement(By.CssSelector("#customer_info_tabs_addresses"));
+            movetoAddress.SendKeys(Keys.Return); //.SendKeys(Keys.Return) is necessary because the element is wrapped in a div or span, I guess.
+
+            var addnewAddress = driver.FindElement(By.CssSelector("#add_address_button"));
+            addnewAddress.Click();
+
+            int count = 1;
+
+            while (IsElementPresent(By.CssSelector("#address_item_shipping_item" + count)) == false) //loop throughs till it find the right CssSelector
             {
-                int complaintNumber = int.Parse(ComplaintNumberx.Text);
+                count = count + 1;
+            }
 
-                string magentouserName = Usernamex.Text;
-                string magentoPass = Passwordx.Password;
-                string workEmail = Emailx.Text;
+            var selectshipAdd = driver.FindElement(By.CssSelector("#address_item_shipping_item" + count));
+            selectshipAdd.Click();
 
-                driver.Url = "http://complaints/Home/ReadOnlyDetails/" + complaintNumber; //opens complaint webpage
+            System.Threading.Thread.Sleep(1000);
 
-                //gets the customers info for replacement orders
+            //start inputting customer's address
 
-                var firstName = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[2]/td[2]")).Text;
+            var fnField = driver.FindElement(By.CssSelector("#_item" + count + "firstname"));
+            fnField.Clear();
+            fnField.SendKeys(firstName);
 
-                var lastName = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[2]/td[3]")).Text;
+            var lnField = driver.FindElement(By.CssSelector("#_item" + count + "lastname"));
+            lnField.Clear();
+            lnField.SendKeys(lastName);
 
-                var email = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[4]/td[1]")).Text;
+            var addstreetAddress = driver.FindElement(By.CssSelector("#_item" + count + "street0"));
+            addstreetAddress.SendKeys(streetAddress);
 
-                var phoneNumber = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[4]/td[2]")).Text;
+            var addCity = driver.FindElement(By.CssSelector("#_item" + count + "city"));
+            addCity.SendKeys(city);
 
-                var country = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[4]/td[4]")).Text;
+            var addState1 = driver.FindElement(By.CssSelector("#_item" + count + "region_id"));
+            addState1.Click();
+            addState1.SendKeys(state);
+            addState1.SendKeys(Keys.Return);
 
-                var streetAddress = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[1]")).Text;
+            //Could add country here...but we only send replacements domestically.
 
-                var city = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[2]")).Text;
+            var addZip = driver.FindElement(By.CssSelector("#_item" + count + "postcode"));
+            addZip.SendKeys(zipcode);
 
-                var state = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[3]")).Text;
+            var addPhone = driver.FindElement(By.CssSelector("#_item" + count + "telephone"));
+            addPhone.SendKeys(phoneNumber);
 
-                var zipcode = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[1]/table/tbody/tr[6]/td[4]")).Text;
+            System.Threading.Thread.Sleep(1000);
 
-                var itemNumber = driver.FindElement(By.XPath("/html/body/div[2]/div/fieldset[2]/table/tbody/tr[2]/td[1]")).Text;
+            var saveandcontinueEdit = driver.FindElement(By.CssSelector("[title^= 'Save and Continue Edit']"));
+            var js = (IJavaScriptExecutor)driver;
+            IJavaScriptExecutor je = (IJavaScriptExecutor)driver; //scrolls up so the element is clickable
+            je.ExecuteScript("arguments[0].scrollIntoView(false);", saveandcontinueEdit);
+            saveandcontinueEdit.Click();
 
-                //Beginning of Magento navigation
 
-                driver.Url = "https://www.bobsredmill.com/index.php/admin/";
-
-                var userName = driver.FindElement(By.XPath("//*[@id='username']")); //if using the app two times in a row this will throw an exception because you're already logged in!
-                userName.SendKeys(magentouserName);
-
-                var password = driver.FindElement(By.XPath("//*[@id='login']"));
-                password.SendKeys(magentoPass);
-
-                var magLogin = driver.FindElement(By.XPath("//*[@id='loginForm']/div/div[5]/input"));
-                magLogin.Click();
-
-                //navigating to one's own account
-
-                System.Threading.Thread.Sleep(2000);
-
-                var customersTab = driver.FindElement(By.XPath("//*[@id='nav']/li[4]"));
-                customersTab.Click();
-
-                System.Threading.Thread.Sleep(3000);
-
-                var manageCustomers = driver.FindElement(By.XPath("//*[@id='nav']/li[4]/ul/li[1]/a"));
-                manageCustomers.Click();
-
-                var csrepemailSearch = driver.FindElement(By.XPath("//*[@id='customerGrid_filter_email']"));
-                csrepemailSearch.SendKeys(workEmail);
-                csrepemailSearch.SendKeys(Keys.Return);
-
-                System.Threading.Thread.Sleep(4000); //NEEDED, otherwise the next line won't click the right element
-
-                var csrepemailEnter = driver.FindElement(By.ClassName("even"));
-                csrepemailEnter.Click();
-
-                var movetoAddress = driver.FindElement(By.CssSelector("#customer_info_tabs_addresses"));
-                movetoAddress.SendKeys(Keys.Return); //.SendKeys(Keys.Return) is necessary because the element is wrapped in a div or span, I guess.
-
-                var addnewAddress = driver.FindElement(By.CssSelector("#add_address_button"));
-                addnewAddress.Click();
-
-                int count = 1;
-
-                while (IsElementPresent(By.CssSelector("#address_item_shipping_item" + count)) == false) //loop throughs till it find the right CssSelector
-                {
-                    count = count + 1;
-                }
-
-                var selectshipAdd = driver.FindElement(By.CssSelector("#address_item_shipping_item" + count));
-                selectshipAdd.Click();
-
-                System.Threading.Thread.Sleep(1000);
-
-                //start inputting customer's address
-
-                var fnField = driver.FindElement(By.CssSelector("#_item" + count + "firstname"));
-                fnField.Clear();
-                fnField.SendKeys(firstName);
-
-                var lnField = driver.FindElement(By.CssSelector("#_item" + count + "lastname"));
-                lnField.Clear();
-                lnField.SendKeys(lastName);
-
-                var addstreetAddress = driver.FindElement(By.CssSelector("#_item" + count + "street0"));
-                addstreetAddress.SendKeys(streetAddress);
-
-                var addCity = driver.FindElement(By.CssSelector("#_item" + count + "city"));
-                addCity.SendKeys(city);
-
-                var addState1 = driver.FindElement(By.CssSelector("#_item" + count + "region_id"));
-                addState1.Click();
-                addState1.SendKeys(state);
-                addState1.SendKeys(Keys.Return);
-
-                //Could add country here...but we only send replacements domestically.
-
-                var addZip = driver.FindElement(By.CssSelector("#_item" + count + "postcode"));
-                addZip.SendKeys(zipcode);
-
-                var addPhone = driver.FindElement(By.CssSelector("#_item" + count + "telephone"));
-                addPhone.SendKeys(phoneNumber);
-
-                System.Threading.Thread.Sleep(1000);
-
-                var saveandcontinueEdit = driver.FindElement(By.CssSelector("[title^= 'Save and Continue Edit']"));
-                var js = (IJavaScriptExecutor)driver;                 
-                IJavaScriptExecutor je = (IJavaScriptExecutor)driver; //scrolls up so the element is clickable
-                je.ExecuteScript("arguments[0].scrollIntoView(false);", saveandcontinueEdit);
-                saveandcontinueEdit.Click();
-
-           
             //maybe add create order
 
             //that's it!
 
         }
-
-     
     }
 }
